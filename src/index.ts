47e3681a5dee557613fc515b6ec5a61cedd50b53
Vayun Soni <vayunsoni@gmail.com>
2023-02-05 20:30:50 +0530

    Spaceship Animation

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,31 @@ const app = new Application({
 	height: window.innerHeight
 });
 
+class Spaceship {
+    private ss: Sprite;
+
+    constructor(app: Application) {
+        this.ss = Sprite.from("spaceship.png")
+        this.ss.anchor.set(0.5);
+        this.ss.x = window.innerHeight/2;
+        this.ss.y = window.innerHeight - 100;
+        this.ss.scale.set(0.11);
+        app.stage.addChild(this.ss)
+    }
+    update(){
+        this.ss.x +=1;
+        if(this.ss.x > window.innerHeight){
+            this.ss.x =0;
+        }
+    }
+
+}
+
+let spaceship: Spaceship;
 
 function init(){
     console.log('Initialising');
+    //background
     const background: Sprite = Sprite.from("background.jpeg");
     background.anchor.set(0);
     background.x = 0;
@@ -19,10 +41,14 @@ function init(){
     background.height = window.innerHeight;
     background.width = window.innerHeight;
     app.stage.addChild(background);
+
+    //spaceship
+    spaceship = new Spaceship(app);
 }
 
 function update(){
     console.log('Updating');
+    spaceship.update();
 
 }
 
