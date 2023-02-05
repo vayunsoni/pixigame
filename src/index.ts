ebab9eefca42a0e8dab05790ccbc5ead6e12d142
Vayun Soni <vayunsoni@gmail.com>
2023-02-05 19:49:40 +0530

    Background set

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,31 @@ const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+    backgroundColor: 0x00F0F,
+	width: window.innerHeight,
+	height: window.innerHeight
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
 
-clampy.anchor.set(0.5);
+function init(){
+    console.log('Initialising');
+    const background: Sprite = Sprite.from("background.jpeg");
+    background.anchor.set(0);
+    background.x = 0;
+    background.y = 0;
+    background.height = window.innerHeight;
+    background.width = window.innerHeight;
+    app.stage.addChild(background);
+}
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+function update(){
+    console.log('Updating');
 
-app.stage.addChild(clampy);
+}
+
+function main(){
+    init();
+    setInterval(update,1000/60);
+}
+
+window.onload = main;
